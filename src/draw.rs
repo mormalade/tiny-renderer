b312560766bc8af694c800fbda9d910cd7868565
mormalade <devbaeb@gmail.com>
2025-10-28 19:06:31 +0300

    drawing wireframe of a 3d model

--- src/draw.rs
+++ src/draw.rs
@@ -1,47 +1,15 @@
 use image::{self, ImageBuffer, Rgba};
 use rand::prelude::*;
 
-#[derive(PartialEq)]
-struct Point {
-    x: u32,
-    y: u32,
+#[derive(Debug, PartialEq)]
+pub struct Point {
+    pub x: u32,
+    pub y: u32,
 }
 
 impl Point {
-    fn new(x: u32, y: u32) -> Self {
+    pub fn new(x: u32, y: u32) -> Self {
         Self { x, y }
-    }
-}
-
-fn line(p1: &Point, p2: &Point, imgbuf: &mut ImageBuffer<Rgba<u8>, Vec<u8>>, color: &Rgba<u8>) {
-    let dx = p1.x as i32 - p2.x as i32;
-    let dy = p1.y as i32 - p2.y as i32;
-    if dx.abs() > dy.abs() {
-        if p1.x > p2.x {
-            let mut y = p2.y as f32;
-            for x in p2.x..p1.x {
-                y += dy as f32 / dx as f32;
-                imgbuf.put_pixel(x, y as u32, *color);
-            }
-        } else {
-            let mut y = p1.y as f32;
-            for x in p1.x..p2.x {
-                y += dy as f32 / dx as f32;
-                imgbuf.put_pixel(x, y as u32, *color);
-            }
-        }
-    } else if p1.y > p2.y {
-        let mut x = p2.x as f32;
-        for y in p2.y..p1.y {
-            x += dx as f32 / dy as f32;
-            imgbuf.put_pixel(x as u32, y, *color);
-        }
-    } else {
-        let mut x = p1.x as f32;
-        for y in p1.y..p2.y {
-            x += dx as f32 / dy as f32;
-            imgbuf.put_pixel(x as u32, y, *color);
-        }
     }
 }
 
@@ -81,24 +49,39 @@ impl Canvas {
     pub fn save(&self, file: &str) {
         self.imgbuf.save(file).unwrap();
     }
-}
-
-pub fn random_lines() {
-    let width = 64;
-    let height = 64;
-
-    let mut imgbuf = ImageBuffer::new(width, height);
-
-    let mut rng = rand::rng();
-
-    for _ in 0..1 << 24 {
-        let a = Point::new(rng.random_range(0..width), rng.random_range(0..height));
-        let b = Point::new(rng.random_range(0..width), rng.random_range(0..height));
-
-        let color: [u8; 4] = [rand::random(), rand::random(), rand::random(), 255];
-        let color = Rgba(color);
-        line(&a, &b, &mut imgbuf, &color);
+    pub fn line(&mut self, p1: &Point, p2: &Point) {
+        let dx = p1.x as i32 - p2.x as i32;
+        let dy = p1.y as i32 - p2.y as i32;
+        if dx.abs() > dy.abs() {
+            if p1.x > p2.x {
+                let mut y = p2.y as f32;
+                for x in p2.x..p1.x {
+                    y += dy as f32 / dx as f32;
+                    self.imgbuf
+                        .put_pixel(x, self.height - 1 - y as u32, self.color);
+                }
+            } else {
+                let mut y = p1.y as f32;
+                for x in p1.x..p2.x {
+                    y += dy as f32 / dx as f32;
+                    self.imgbuf
+                        .put_pixel(x, self.height - 1 - y as u32, self.color);
+                }
+            }
+        } else if p1.y > p2.y {
+            let mut x = p2.x as f32;
+            for y in p2.y..p1.y {
+                x += dx as f32 / dy as f32;
+                self.imgbuf
+                    .put_pixel(x as u32, self.height - 1 - y, self.color);
+            }
+        } else {
+            let mut x = p1.x as f32;
+            for y in p1.y..p2.y {
+                x += dx as f32 / dy as f32;
+                self.imgbuf
+                    .put_pixel(x as u32, self.height - 1 - y, self.color);
+            }
+        }
     }
-
-    imgbuf.save("lines.tga").unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod draw;
 
-use draw::Canvas;
+use draw::{Canvas, Point};
 use std::{fs::File, io::Read};
 
 #[derive(Debug)]
@@ -38,7 +38,10 @@ fn main() {
     let mut buf = String::new();
     _ = model.read_to_string(&mut buf).unwrap();
 
-    let mut vertices: Vec<Vertex> = Vec::new();
+    let mut canvas = Canvas::new(1024, 1024);
+    canvas.set_color(255, 0, 0);
+
+    let mut vertices: Vec<draw::Point> = Vec::new();
     let mut triangles: Vec<Triangle> = Vec::new();
     // make a hashmap??
     for line in buf.lines() {
@@ -51,9 +54,15 @@ fn main() {
             "v" => {
                 let x: f32 = line.next().unwrap().parse().unwrap();
                 let y: f32 = line.next().unwrap().parse().unwrap();
+                // I don't use z for now
                 let z: f32 = line.next().unwrap().parse().unwrap();
-                let vertex = Vertex::new(x, y, z);
-                vertices.push(vertex);
+
+                // -1.0 to 1.0 converted into 0 to canvas.width
+                // (0 to 2) / 2 is the same as -1 to 1
+                let x = ((x + 1.) * (canvas.width - 1) as f32 / 2.) as u32;
+                let y = ((y + 1.) * (canvas.height - 1) as f32 / 2.) as u32;
+                let point = Point::new(x, y);
+                vertices.push(point);
             }
             "f" => {
                 // divide by / and then push to faces
@@ -87,16 +96,16 @@ fn main() {
             _ => (),
         }
     }
-    // draw points
-    let mut canvas = Canvas::new(1024, 1024);
-    canvas.set_color(255, 0, 0);
 
-    for vertex in vertices {
-        // -1.0 to 1.0 converted into 0 to 64
-        // (0 to 2) / 2 is the same
-        let x = ((vertex.x + 1.) * (canvas.width - 1) as f32 / 2.) as u32;
-        let y = ((vertex.y + 1.) * (canvas.height - 1) as f32 / 2.) as u32;
-        canvas.put_pixel(x, canvas.height - 1 - y);
+    // draw triangles
+    for trinagle in triangles {
+        let p1 = &vertices[trinagle.a];
+        let p2 = &vertices[trinagle.b];
+        let p3 = &vertices[trinagle.c];
+
+        canvas.line(p1, p2);
+        canvas.line(p2, p3);
+        canvas.line(p1, p3);
     }
 
     canvas.save("diablo.tga");
