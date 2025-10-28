e84e314445d4c16f3b2dbe21275b26b1f7351995
mormalade <devbaeb@gmail.com>
2025-10-28 21:10:32 +0300

    drawing wireframe of a 3d model

--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,14 @@
 mod draw;
+mod parser;
 
-use draw::{Canvas, Point};
-use std::{fs::File, io::Read};
-
-#[derive(Debug)]
-struct Vertex {
-    x: f32,
-    y: f32,
-    z: f32,
-}
-
-impl Vertex {
-    fn new(x: f32, y: f32, z: f32) -> Self {
-        Self { x, y, z }
-    }
-}
-
-#[derive(Debug)]
-struct Triangle {
-    a: usize,
-    b: usize,
-    c: usize,
-}
-
-impl Triangle {
-    fn new(a: usize, b: usize, c: usize) -> Self {
-        Self {
-            a: a - 1,
-            b: b - 1,
-            c: c - 1,
-        }
-    }
-}
+use draw::Canvas;
+use parser::parse_obj;
 
 fn main() {
-    let mut model = File::open("src/diablo3_pose.obj").expect("file should be there");
-    let mut buf = String::new();
-    _ = model.read_to_string(&mut buf).unwrap();
-
-    let mut canvas = Canvas::new(1024, 1024);
+    let mut canvas = Canvas::new(64, 64);
     canvas.set_color(255, 0, 0);
 
-    let mut vertices: Vec<draw::Point> = Vec::new();
-    let mut triangles: Vec<Triangle> = Vec::new();
-    // make a hashmap??
-    for line in buf.lines() {
-        let mut line = line.split_whitespace();
-        let data_type = match line.next() {
-            Some(data) => data,
-            None => continue,
-        };
-        match data_type {
-            "v" => {
-                let x: f32 = line.next().unwrap().parse().unwrap();
-                let y: f32 = line.next().unwrap().parse().unwrap();
-                // I don't use z for now
-                let z: f32 = line.next().unwrap().parse().unwrap();
-
-                // -1.0 to 1.0 converted into 0 to canvas.width
-                // (0 to 2) / 2 is the same as -1 to 1
-                let x = ((x + 1.) * (canvas.width - 1) as f32 / 2.) as u32;
-                let y = ((y + 1.) * (canvas.height - 1) as f32 / 2.) as u32;
-                let point = Point::new(x, y);
-                vertices.push(point);
-            }
-            "f" => {
-                // divide by / and then push to faces
-                let a: usize = line
-                    .next()
-                    .unwrap()
-                    .split('/')
-                    .next()
-                    .unwrap()
-                    .parse()
-                    .unwrap();
-                let b: usize = line
-                    .next()
-                    .unwrap()
-                    .split('/')
-                    .next()
-                    .unwrap()
-                    .parse()
-                    .unwrap();
-                let c: usize = line
-                    .next()
-                    .unwrap()
-                    .split('/')
-                    .next()
-                    .unwrap()
-                    .parse()
-                    .unwrap();
-                let triangle = Triangle::new(a, b, c);
-                triangles.push(triangle);
-            }
-            _ => (),
-        }
-    }
+    let (vertices, triangles) = parse_obj("src/diablo3_pose.obj", &canvas);
 
     // draw triangles
     for trinagle in triangles {
@@ -108,5 +21,5 @@ fn main() {
         canvas.line(p1, p3);
     }
 
-    canvas.save("diablo.tga");
+    canvas.save("triangles.tga");
 }
---
+++ src/parser.rs
@@ -0,0 +1,81 @@
+use crate::draw::{Canvas, Point};
+use std::{fs::File, io::Read};
+
+#[derive(Debug)]
+pub struct Triangle {
+    pub a: usize,
+    pub b: usize,
+    pub c: usize,
+}
+
+impl Triangle {
+    fn new(a: usize, b: usize, c: usize) -> Self {
+        Self {
+            a: a - 1,
+            b: b - 1,
+            c: c - 1,
+        }
+    }
+}
+
+pub fn parse_obj(file: &str, canvas: &Canvas) -> (Vec<Point>, Vec<Triangle>) {
+    let mut model = File::open(file).expect("file should be there");
+    let mut buf = String::new();
+    _ = model.read_to_string(&mut buf).unwrap();
+
+    let mut vertices: Vec<Point> = Vec::new();
+    let mut triangles: Vec<Triangle> = Vec::new();
+
+    for line in buf.lines() {
+        let mut line = line.split_whitespace();
+        let data_type = match line.next() {
+            Some(data) => data,
+            None => continue,
+        };
+        match data_type {
+            "v" => {
+                let x: f32 = line.next().unwrap().parse().unwrap();
+                let y: f32 = line.next().unwrap().parse().unwrap();
+                // I don't use z for now
+                let z: f32 = line.next().unwrap().parse().unwrap();
+
+                // -1.0 to 1.0 converted into 0 to canvas.width
+                // (0 to 2) / 2 is the same as -1 to 1
+                let x = ((x + 1.) * (canvas.width - 1) as f32 / 2.) as u32;
+                let y = ((y + 1.) * (canvas.height - 1) as f32 / 2.) as u32;
+                let point = Point::new(x, y);
+                vertices.push(point);
+            }
+            "f" => {
+                let a: usize = line
+                    .next()
+                    .unwrap()
+                    .split('/')
+                    .next()
+                    .unwrap()
+                    .parse()
+                    .unwrap();
+                let b: usize = line
+                    .next()
+                    .unwrap()
+                    .split('/')
+                    .next()
+                    .unwrap()
+                    .parse()
+                    .unwrap();
+                let c: usize = line
+                    .next()
+                    .unwrap()
+                    .split('/')
+                    .next()
+                    .unwrap()
+                    .parse()
+                    .unwrap();
+                let triangle = Triangle::new(a, b, c);
+                triangles.push(triangle);
+            }
+            _ => (),
+        }
+    }
+    (vertices, triangles)
+}
